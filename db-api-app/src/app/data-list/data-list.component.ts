a1badafe712a882230d4f680bf02af59e8c922f8
Alberto Santacruz <mirasmar.dev@gmail.com>
2024-05-02 14:22:14 +0200

    Created "Search" component

--- db-api-app/src/app/data-list/data-list.component.ts
+++ db-api-app/src/app/data-list/data-list.component.ts
@@ -6,9 +6,13 @@ import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort, Sort } from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { DataDetailsComponent } from '../data-details/data-details.component';
+import { SearchFilterComponent } from '../search-filter/search-filter.component';
+import { FilterService } from '../services/filter-service/filter.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-data-list',
@@ -22,30 +26,60 @@ import { DataDetailsComponent } from '../data-details/data-details.component';
     MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCheckboxModule,
     DataDetailsComponent,
+    SearchFilterComponent,
   ],
 })
 export class DataListComponent implements OnInit, AfterViewInit {
+  private unsubscribe = new Subject<void>();
+
   retailers: Retailer[] = [];
+  filteredRetailers: Retailer[] = [];
   selectedRetailer: any;
   columns: string[] = ['code', 'name', 'country', 'scheme'];
   dataSource = new MatTableDataSource<Retailer>();
+  filters: any = {
+    code: { checked: false, value: '' },
+    name: { checked: false, value: '' },
+    country: { checked: false, value: '' },
+    scheme: { checked: false, value: '' },
+  };
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private dbDataApiService: DbDataApiService,
-    private _liveAnnouncer: LiveAnnouncer
+    private _liveAnnouncer: LiveAnnouncer,
+    private filterService: FilterService
   ) {}
 
   ngOnInit(): void {
     this.fetchData();
+
+    this.filterService
+      .getSearchData$()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((data) => {
+        this.dataSource.filter = data;
+      });
+    this.filterService
+      .getFilteredData$()
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe((filteredData) => {
+        this.dataSource.data = filteredData;
+      });
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   fetchData(): void {
@@ -66,13 +100,6 @@ export class DataListComponent implements OnInit, AfterViewInit {
     } else {
       this._liveAnnouncer.announce('Sorting cleared');
     }
-  }
-
-  applyFilter(event: KeyboardEvent) {
-    let filterValue = (event.target as HTMLInputElement).value;
-    filterValue = filterValue.trim();
-    filterValue = filterValue.toLowerCase();
-    this.dataSource.filter = filterValue;
   }
 
   onRowClicked(row: any) {
--- db-api-app/src/app/search-filter/search-filter.component.ts
+++ db-api-app/src/app/search-filter/search-filter.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FilterService } from '../services/filter-service/filter.service';
 
 @Component({
   selector: 'app-search-filter',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+  ],
   templateUrl: './search-filter.component.html',
-  styleUrl: './search-filter.component.scss'
+  styleUrl: './search-filter.component.scss',
 })
 export class SearchFilterComponent {
+  @Input() data: any[] = [];
+  @Input() columns: string[] = [];
+  @Input() filters: any;
 
+  filteredData: any[] = [];
+
+  constructor(private filterService: FilterService) {}
+
+  setSearchValue(event: KeyboardEvent): void {
+    let valueToSearch = (event.target as HTMLInputElement).value;
+    valueToSearch = valueToSearch.trim();
+    valueToSearch = valueToSearch.toLowerCase();
+
+    this.filterService.setSearchData(valueToSearch);
+  }
+
+  filterBy(event: any, filter: string): void {
+    this.filters[filter].checked = !this.filters[filter].checked;
+    this.filters[filter].value = !event.checked && '';
+  }
+
+  saveFilterValue(event: KeyboardEvent, filter: string) {
+    let valueToSearch = (event.target as HTMLInputElement).value;
+    valueToSearch = valueToSearch.trim();
+    valueToSearch = valueToSearch.toLowerCase();
+
+    if (valueToSearch !== '' && this.filters[filter].checked) {
+      this.filters[filter].value = valueToSearch;
+    }
+  }
+
+  setFilteredData(): void {
+    this.filteredData = [...this.data];
+
+    for (let key in this.filters) {
+      if (this.filters[key].checked) {
+        this.filteredData = this.filteredData.filter((dataRow) =>
+          dataRow[key as keyof any]
+            .toLowerCase()
+            .includes(this.filters[key].value)
+        );
+      }
+    }
+
+    this.filterService.setFilteredData(this.filteredData);
+  }
 }
