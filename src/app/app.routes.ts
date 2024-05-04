da5fcc53d3c08b6ec988ce6d17e068f20ba72087
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-04 15:08:56 +0200

    refactor: Deleted html and scss of app component
    - Added app routes.
    - Main component for Layout component
    - Configured HttpClient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './domains/shared/components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./domains/load-profiles/pages/list/list.component')
+      }
+    ]
+
+  }
+];
