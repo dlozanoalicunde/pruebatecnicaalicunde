c850c2a2c8c74ada5d626346d314a15026f9eeeb
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-04 15:11:05 +0200

    feat(list): Added getMbaOptions of loadProfiles service
    Configured method with call the getMba.

--- src/app/domains/load-profiles/pages/list/list.component.ts
+++ src/app/domains/load-profiles/pages/list/list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { LoadProfileService } from '@shared/services/load-profile.service';
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export default class ListComponent {
 
+  private loadProfileService = inject(LoadProfileService);
+
+  ngOnInit(): void {
+    this.getMbaOptions();
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
+
+  private getMbaOptions(){
+    this.loadProfileService.getMba().subscribe({
+      next: (mbaOptions) => {
+        console.log(mbaOptions);
+      },
+      error: () => {
+
+      }
+    })
+  }
 }
