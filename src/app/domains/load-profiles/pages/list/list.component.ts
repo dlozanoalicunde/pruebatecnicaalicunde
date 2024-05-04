16f231152ae45dc0b7615b641c56c0a7d991aff5
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-04 16:29:50 +0200

    feat(list): Added mat-select and data the service getMba
    Added list mba data service

--- src/app/domains/load-profiles/pages/list/list.component.ts
+++ src/app/domains/load-profiles/pages/list/list.component.ts
@@ -1,18 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { LoadProfileService } from '@shared/services/load-profile.service';
-
+import {MatSelectModule} from '@angular/material/select';
+import { MbaOptions } from '@models/mba-options.model';
+import { Mba } from '@models/mba.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSelectModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export default class ListComponent {
 
+  respMbaOption = signal<MbaOptions[]>([]);
+  respMbaList = signal<Mba[]>([]);
   private loadProfileService = inject(LoadProfileService);
+
 
   ngOnInit(): void {
     this.getMbaOptions();
@@ -25,6 +30,11 @@ export default class ListComponent {
     this.loadProfileService.getMba().subscribe({
       next: (mbaOptions) => {
         console.log(mbaOptions);
+        this.respMbaOption.set(mbaOptions);
+        if(mbaOptions[0].mbas.length > 0){
+          this.respMbaList.set(mbaOptions[0].mbas);
+        }
+
       },
       error: () => {
 
