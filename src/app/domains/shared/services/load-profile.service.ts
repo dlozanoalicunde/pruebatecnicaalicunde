4f4b831afa5f45d7fccb9fa249e1decce854171b
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-04 15:03:42 +0200

    feat(service): Added getMba method
    - Cofigured environment api with endpoint.

--- src/app/domains/shared/services/load-profile.service.ts
+++ src/app/domains/shared/services/load-profile.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+import { environment } from '@env/environment';
+import { MbaOptions } from '@models/mba-options.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadProfileService {
 
+  private http = inject(HttpClient);
+  api = 'EXP18/MBAOptions'
+
   constructor() { }
+
+  getMba(){
+    return this.http.get<MbaOptions[]>(`${environment.API_URL}${this.api}`);
+  }
 }
