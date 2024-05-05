a10fd9d8b69e3a858210d2b8ad57389680ed6371
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-05 18:00:25 +0200

    feat(services): Added aggregate service
    #Explanation
    - Added getAggregate method with params for service.

--- src/app/domains/shared/services/load-profile.service.ts
+++ src/app/domains/shared/services/load-profile.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, Signal, inject } from '@angular/core';
 
 import { environment } from '@env/environment';
+import { LoadProfile } from '@models/load-profile.model';
 import { MbaOptions } from '@models/mba-options.model';
 
 
@@ -11,11 +12,18 @@ import { MbaOptions } from '@models/mba-options.model';
 export class LoadProfileService {
 
   private http = inject(HttpClient);
-  api = 'EXP18/MBAOptions'
+  api = 'EXP18/'
 
   constructor() { }
 
   getMba(){
-    return this.http.get<MbaOptions[]>(`${environment.API_URL}${this.api}`);
+    return this.http.get<MbaOptions[]>(`${environment.API_URL}${this.api}MBAOptions`);
+  }
+
+  getAggregate(end: string, mba: string, mga: string, resolution: string, start: string) {
+    const url = `${environment.API_URL}${this.api}Aggregate`;
+    const params = `?end=${end}&mba=${mba}&mga=${mga}&resolution=${resolution}&start=${start}`;
+
+    return this.http.get<LoadProfile[]>(url + params);
   }
 }
