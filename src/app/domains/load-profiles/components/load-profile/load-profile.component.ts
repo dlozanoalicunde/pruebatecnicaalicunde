d655213716155f7b26c5bb6ea919f5b5c5c36a4b
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-05 18:12:07 +0200

    refactor: divide code into smart and dumb components for list and load-profile
    # Explanation
    - Added ViewChild element for selecting the resolution.
    - Implemented an array to store resolution options.
    - Added a form for selecting start and end dates.
    - Integrated signals for managing resolution, MBA options, and load profiles.

--- src/app/domains/load-profiles/components/load-profile/load-profile.component.ts
+++ src/app/domains/load-profiles/components/load-profile/load-profile.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component, Input } from '@angular/core';
+import { Mba } from '@models/mba.model';
+import { LoadProfile } from '@models/load-profile.model';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FilterOptionsPipe } from '@shared/pipes/filter-options.pipe';
+import { Resolution } from '@models/resolution.model';
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
 
 @Component({
   selector: 'app-load-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSelectModule, MatDatepickerModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, FilterOptionsPipe],
   templateUrl: './load-profile.component.html',
   styleUrl: './load-profile.component.scss'
 })
 export class LoadProfileComponent {
+
+  @Input() mbaOptions: Mba[] = [];
+  @Input() loadProfiles: LoadProfile[] = [];
+  @Input({required: true}) selectResolution!: string;
+
+  selectedOption: string | undefined;
+  searchQuery: string = '';
+  campaignOne = new FormGroup({
+    start: new FormControl(new Date(year, month, 13)),
+    end: new FormControl(new Date(year, month, 16)),
+  });
+  campaignTwo = new FormGroup({
+    start: new FormControl(new Date(year, month, 15)),
+    end: new FormControl(new Date(year, month, 19)),
+  });
+
+  resolutions = [
+    {value: 'year', viewValue: 'Year'},
+    {value: 'month', viewValue: 'Month'},
+    {value: 'week', viewValue: 'Week'},
+    {value: 'hour', viewValue: 'Hour'},
+  ];
+
+  selectionChange(e: any) {
+    const newResolution = e.value.toString();
+    // selectResolution.set(newResolution);
+    // this.resolutionSignal.set(newResolution);
+    // this.getAggregate();
+  }
 
 }
--- src/app/domains/load-profiles/pages/list/list.component.ts
+++ src/app/domains/load-profiles/pages/list/list.component.ts
@@ -1,44 +1,120 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject, signal } from '@angular/core';
 import { LoadProfileService } from '@shared/services/load-profile.service';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MbaOptions } from '@models/mba-options.model';
 import { Mba } from '@models/mba.model';
+import { LoadProfile } from '@models/load-profile.model';
+
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Resolution } from '@models/resolution.model';
+import { FilterSelectDirective } from '@shared/directives/filter-select.directive';
+import { FilterOptionsPipe } from '@shared/pipes/filter-options.pipe';
+import { LoadProfileComponent } from '../../components/load-profile/load-profile.component';
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, MatSelectModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [CommonModule, MatSelectModule, MatDatepickerModule, MatFormFieldModule, FormsModule,
+    ReactiveFormsModule, FilterSelectDirective, FilterOptionsPipe, LoadProfileComponent] ,
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export default class ListComponent {
 
+  @ViewChild("changeEl") el!: ElementRef;
+
+  private loadProfileService = inject(LoadProfileService);
   respMbaOption = signal<MbaOptions[]>([]);
   respMbaList = signal<Mba[]>([]);
-  private loadProfileService = inject(LoadProfileService);
+  respLoadProfile = signal<LoadProfile[]>([]);
+  resolutions: Resolution[] = [
+    {value: 'year', viewValue: 'Year'},
+    {value: 'month', viewValue: 'Month'},
+    {value: 'week', viewValue: 'Week'},
+    {value: 'hour', viewValue: 'Hour'},
+  ];
+  selectedOption: string | undefined;
+  searchQuery: string = '';
+  resolution: string = 'month';
+  resolutionSignal = signal<string>('month');
+  selectedMba:any;
+
+  campaignOne = new FormGroup({
+    start: new FormControl(new Date(year, month, 13)),
+    end: new FormControl(new Date(year, month, 16)),
+  });
+  campaignTwo = new FormGroup({
+    start: new FormControl(new Date(year, month, 15)),
+    end: new FormControl(new Date(year, month, 19)),
+  });
 
 
+  ngAfterViewInit(): void {
+    this.resolutionSignal.set(this.resolutions[1].value);
+
+    setTimeout(() => {
+      this.getAggregate();
+    });
+  }
   ngOnInit(): void {
-    this.getMbaOptions();
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    this.resolutionSignal.set(this.resolutions[1].value);
 
+    this.getMbaOptions();
   }
 
-  private getMbaOptions(){
+  private getAggregate() {
+    const start = this.campaignOne?.get('start')?.value;
+    const end = this.campaignOne?.get('end')?.value;
+    console.log('ingreso getAggregate end: ',end,);
+
+    const mba = '10Y1001A1001A44P';
+    const mga = 'ALS';
+
+    if (start && end) {
+      this.loadProfileService.getAggregate(end.toISOString(), 'string', 'ALS', this.resolutionSignal(), end.toISOString()).subscribe({
+        next: (aggregate) => {
+          console.log("aggregate ", aggregate);
+          this.respLoadProfile.set(aggregate);
+        },
+        error: () => {
+
+        }
+      });
+
+    } else {
+      console.log('no ingreso');
+    }
+  }
+
+  private getMbaOptions() {
     this.loadProfileService.getMba().subscribe({
       next: (mbaOptions) => {
         console.log(mbaOptions);
         this.respMbaOption.set(mbaOptions);
-        if(mbaOptions[0].mbas.length > 0){
-          this.respMbaList.set(mbaOptions[0].mbas);
+        if (mbaOptions[0].mbas.length > 0) {
+          this.respMbaList.set(mbaOptions[0].mbas)
         }
 
       },
       error: () => {
 
       }
-    })
+    });
   }
+
+  selectionChange(e: any) {
+    const newResolution = e.value.toString();
+    this.resolutionSignal.set(newResolution);
+    this.getAggregate();
+  }
+
 }
