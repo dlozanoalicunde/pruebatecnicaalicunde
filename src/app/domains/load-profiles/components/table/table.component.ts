95d66ede885858ba53936186a8bd662fae5bae47
Ingrid Ruiz <ingrijrm94@gmail.com>
2024-05-06 11:30:36 +0200

    feat(components): Added table response aggregate service
    -Added input for pass data between load-profile (father) and table (child)
    -Configured date with dataSource for angular material and filter.

---
+++ src/app/domains/load-profiles/components/table/table.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatSort } from '@angular/material/sort';
+import { CommonModule } from '@angular/common';
+import { LoadProfile } from '@models/load-profile.model';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatTableModule],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss'
+})
+export class TableComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('sortAggregate') sortAggregate!: MatSort;
+
+  @Input() aggregateList!: LoadProfile[];
+  public displayedColumns: string[] = ['timestamp', 'timestampUTC', 'mgaCode', 'mgaName', 'mba', 'quantity'];
+  public dataSource: MatTableDataSource<LoadProfile> = new MatTableDataSource<LoadProfile>();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.dataSource = new MatTableDataSource(this.aggregateList);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sortAggregate;
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sortAggregate;
+  }
+
+  public applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.firstPage();
+  }
+
+  public firstPage(): void {
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
